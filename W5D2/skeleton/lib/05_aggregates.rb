007f0b3bdaaf5ce396b3510778b8d26cc4dfdb31
Bryan <blindaa121@gmail.com>
2020-03-10 17:58:36 -0700

    W5D2 Project Submissions

---
+++ W5D2/skeleton/lib/05_aggregates.rb
@@ -0,0 +1,102 @@
+# == Schema Information
+#
+# Table name: countries
+#
+#  name        :string       not null, primary key
+#  continent   :string
+#  area        :integer
+#  population  :integer
+#  gdp         :integer
+
+require_relative './sqlzoo.rb'
+
+def example_sum
+  execute(<<-SQL)
+    SELECT
+      SUM(population)
+    FROM
+      countries
+  SQL
+end
+
+def continents
+  # List all the continents - just once each.
+  execute(<<-SQL)
+  select 
+    DISTINCT continent
+  from
+    countries
+  ; 
+  SQL
+end
+
+def africa_gdp
+  # Give the total GDP of Africa.
+  execute(<<-SQL)
+  select 
+    SUM(gdp)
+  from 
+    countries
+  where
+    continent = 'Africa'
+  SQL
+end
+
+def area_count
+  # How many countries have an area of more than 1,000,000?
+  execute(<<-SQL)
+  SELECT
+    count(*)
+  FROM
+    countries
+  WHERE
+  area > 1000000
+  ;
+  SQL
+end
+
+def group_population
+  # What is the total population of ('France','Germany','Spain')?
+  execute(<<-SQL)
+  SELECT
+    SUM(population)
+  FROM
+    countries
+  WHERE
+    name = 'France' or name = 'Germany' or name = 'Spain'
+  SQL
+end
+
+def country_counts
+  # For each continent show the continent and number of countries.
+  execute(<<-SQL)
+  SELECT
+    continent, count(*)
+  FROM
+    countries
+  GROUP BY 
+    continent
+  SQL
+end
+
+def populous_country_counts
+  # For each continent show the continent and number of countries with
+  # populations of at least 10 million.
+  execute(<<-SQL)
+  SELECT
+    continent, count(population)
+  FROM
+    countries
+  GROUP BY 
+    continent
+  HAVING 
+    min(population) >= 1000000
+    
+  SQL
+end
+
+def populous_continents
+  # List the continents that have a total population of at least 100 million.
+  execute(<<-SQL)
+  SQL
+end
